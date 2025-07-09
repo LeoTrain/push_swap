521c7d7dee3024ea251d7a47a085016587e7c5bc
42_leo <leberton@student.42vienna.com>
2025-07-09 11:45:47 +0200

    Normalize files to norm

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: leberton <leberton@42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:10:34 by leberton          #+#    #+#             */
-/*   Updated: 2025/07/01 20:23:46 by leberton         ###   ########.fr       */
+/*   Updated: 2025/07/09 11:44:38 by leberton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,42 +37,6 @@ static void	sort_three_elements(t_list **list)
 	}
 	else if (first < second && second > third && first > third)
 		rotate_reverse_a(list);
-}
-
-static int	find_min_index(t_list *list)
-{
-	int		i = 0;
-	int		min_index = -1;
-	int		min_value = INT_MAX;
-	t_list	*current = list;
-
-	while (current)
-	{
-		if ((int)current->content < min_value)
-		{
-			min_value = (int)current->content;
-			min_index = i;
-		}
-		current = current->next;
-		i++;
-	}
-	return (min_index);
-}
-
-static void	push_min_to_b(t_list **list_a, t_list **list_b)
-{
-	int		min_index;
-	int		size;
-	
-	min_index = find_min_index(*list_a);
-	size = ft_lstsize(*list_a);
-	if (min_index <= size / 2)
-		while (min_index-- > 0)
-			rotate_a(list_a);
-	else
-		while (min_index++ < size)
-			rotate_reverse_a(list_a);
-	push_b(list_a, list_b);
 }
 
 void	sort_five_elements(t_list **a, t_list **b)
