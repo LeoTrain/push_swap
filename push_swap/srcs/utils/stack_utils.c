b067feb8d9c7c8d2d23c124bcc4d5c3e3ac8060c
leberton_asahi <leberton@asahi.leo>
2025-09-04 02:03:10 +0200

    Added small sort

--- push_swap/srcs/utils/stack_utils.c
+++ push_swap/srcs/utils/stack_utils.c
@@ -1,4 +1,5 @@
 #include "../../includes/push_swap.h"
+#include <stdlib.h>
 
 int	stack_size(t_stack *stack)
 {
@@ -20,4 +21,17 @@ t_stack	*get_last(t_stack *stack)
 	while (stack->next)
 		stack = stack->next;
 	return (stack);
+}
+
+int	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (EXIT_SUCCESS);
+	while (stack->next)
+	{
+		if (stack->index > stack->next->index)
+			return (EXIT_FAILURE);
+		stack = stack->next;
+	}
+	return (EXIT_SUCCESS);
 }
